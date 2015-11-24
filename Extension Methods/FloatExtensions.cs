115fdcf7818b254d92289679d292e1effdf800ad
SanBen <benjamin.pooles@gmail.com>
2015-11-24 23:47:58 +0100

    Added float extensions and expanded vector extensions

---
+++ Extension Methods/FloatExtensions.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Extensions
+{
+    public static class FloatExtensions
+    {
+        public static bool Approximately(this float inst, float compareTo, float tolerance = 0.00001f)
+        {
+            return (Mathf.Abs(inst - compareTo) < tolerance);
+        }
+    }
+}
--- Extension Methods/VectorExtensions.cs
+++ Extension Methods/VectorExtensions.cs
@@ -2,10 +2,9 @@
 
 namespace Extensions
 {
+    //____________________Vector4____________________//
     public static class VectorExtensions
     {
-
-
         public static Vector4 WithX(this Vector4 inst, float x)
         {
             inst.x = x;
@@ -30,8 +29,20 @@ namespace Extensions
             return inst;
         }
 
+        /// <summary>
+        /// Compares vectors with an approximation defined by tolerance
+        /// </summary>
+        /// <param name="inst"></param>
+        /// <param name="toCompare">Vector to compare to</param>
+        /// <param name="tolerance">Tolerance for the approximation</param>
+        /// <returns>Are the vectors approximately the same defined by tolerance?</returns>
+        public static bool Approximately(this Vector4 inst, Vector toCompare, float tolerance = 0.00001f)
+        {
+            return toCompare.Approximately(inst, tolerance);
+        }
 
 
+        //____________________Vector3____________________//
 
         public static Vector3 WithX(this Vector3 inst, float x)
         {
@@ -52,8 +63,22 @@ namespace Extensions
         }
 
 
+        /// <summary>
+        /// Compares vectors with an approximation defined by tolerance
+        /// </summary>
+        /// <param name="inst"></param>
+        /// <param name="toCompare">Vector to compare to</param>
+        /// <param name="tolerance">Tolerance for the approximation</param>
+        /// <returns>Are the vectors approximately the same defined by tolerance?</returns>
+        public static bool Approximately(this Vector3 inst, Vector toCompare, float tolerance = 0.00001f)
+        {
+            return toCompare.Approximately(inst, tolerance);
+        }
 
 
+
+
+        //____________________Vector2____________________//
         public static Vector2 WithX(this Vector2 inst, float x)
         {
             inst.x = x;
@@ -66,5 +91,62 @@ namespace Extensions
             return inst;
         }
 
+        /// <summary>
+        /// Compares vectors with an approximation defined by tolerance
+        /// </summary>
+        /// <param name="inst"></param>
+        /// <param name="toCompare">Vector to compare to</param>
+        /// <param name="tolerance">Tolerance for the approximation</param>
+        /// <returns>Are the vectors approximately the same defined by tolerance?</returns>
+        public static bool Approximately(this Vector2 inst, Vector toCompare, float tolerance = 0.00001f)
+        {
+            return toCompare.Approximately(inst, tolerance);
+        }
+
+
+
+
+        //____________________Generic____________________//
+
+        public static bool Approximately(this Vector inst, Vector toCompare, float tolerance = 0.00001f)
+        {
+            return inst.x.Approximately(toCompare.x, tolerance) &&
+                    inst.y.Approximately(toCompare.y, tolerance) &&
+                    inst.z.Approximately(toCompare.z, tolerance) &&
+                    inst.w.Approximately(toCompare.w, tolerance);
+        }
+
+        public class Vector
+        {
+            public float x;
+            public float y;
+            public float z;
+            public float w;
+
+            public Vector(float x, float y, float z = 0, float w = 0)
+            {
+                this.x = x;
+                this.y = y;
+                this.z = z;
+                this.w = w;
+            }
+
+
+            static public implicit operator Vector(Vector2 value)
+            {
+                return new Vector(value.x, value.y);
+            }
+
+            static public implicit operator Vector(Vector3 value)
+            {
+                return new Vector(value.x, value.y, value.z);
+            }
+
+            static public implicit operator Vector(Vector4 value)
+            {
+                return new Vector(value.x, value.y, value.z, value.w);
+            }
+
+        }
     }
 }
